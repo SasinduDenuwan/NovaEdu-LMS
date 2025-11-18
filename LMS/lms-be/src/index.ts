5e87ccd15638ac473c7c96e879c1b9196c92e4df
SasinduDenuwan <sasindudenuwan2006wpsk@gmail.com>
2025-11-18 15:47:31 +0530

    fix the dependencies

--- LMS/lms-be/src/index.ts
+++ LMS/lms-be/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
+import authRouter from './routes/auth.route';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 dotenv.config();
 
 const PORT = process.env.PORT
@@ -11,6 +13,23 @@ const app = express();
 app.use(express.json());
 app.use(
     cors({
-
+        origin: ["http://localhost:5173"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
     })
 )
+
+app.use("/api/v1/auth", authRouter)
+
+mongoose
+    .connect(MONGO_URI)
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+        console.error(err)
+        process.exit(1)
+    })
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
